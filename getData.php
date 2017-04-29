562f78db0deae5087c6324e9c400f7f267e70866
Ibrahim Bakhsh <igbmadinah@gmail.com>
2017-04-29 16:18:26 +0300

    marker based on opening data

--- getData.php
+++ getData.php
@@ -32,7 +32,7 @@ if (in_array($action, $all_actions) && is_json($params)){
       $lng2 = $params["lng"];
       $where = "(LONGITUDE >= $lat1 AND LONGITUDE <= $lat2) AND (LATITUDE >= $lng1 AND LATITUDE <= $lng2)";
       $where = "1=1";
-      $getallLocations =  $db->getRows("SELECT * FROM `GEO_DATA` WHERE $where order by LONGITUDE desc limit 5"); 
+      $getallLocations =  $db->getRows("SELECT * FROM `GEO_DATA` WHERE ID in(14,11,6,16,15) order by LONGITUDE desc limit 5"); 
       $response["data"] = $getallLocations;
     }
 } else {
